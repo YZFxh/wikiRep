b9fe6d29e6555cf39e858c93b7f689b0cb9185ff
余泽斐 <1136248005@qq.com>
2024-03-24 22:55:58 +0800

    增加了all.sql并集成了mybatis（pom文件中增加依赖，并配置数据源）

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello}")
+    private String hi;
 
     @RequestMapping("/hello1")
     public String hello(){
+        System.out.println("查到的值是"+hi);
         return  "hello";
     }
 }

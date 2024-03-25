23a89d1283d0e986a8185c522c50f36fc9a05ee0
余泽斐 <1136248005@qq.com>
2024-03-25 20:13:36 +0800

    3-5生成ebook接口

---
+++ src/main/java/com/jiawa/wiki/controller/EbookController.java
@@ -0,0 +1,25 @@
+package com.jiawa.wiki.controller;
+
+import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.service.EbookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+    @Autowired
+    private EbookService ebookService;
+
+
+
+    @GetMapping("/list1")
+    public List<Ebook> list() {
+        return ebookService.list();
+    }
+}
